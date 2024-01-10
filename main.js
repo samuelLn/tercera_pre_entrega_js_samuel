3201786ea6f0a5660cb5492d9af6c5eaf60d1658
Samuel <samuel.lara.navarro@gmail.com>
2024-01-10 20:06:32 -0300

    ajustes genereales

---
+++ main.js
@@ -0,0 +1,8 @@
+//import {app} from "./src/app";
+//app();
+
+//import { name} from "./app.js";
+import { saludoInicial} from './app.js'
+import { saludoInicialDos} from './src/appCopy.js'
+saludoInicial();
+saludoInicialDos();
